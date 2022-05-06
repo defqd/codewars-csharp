de2a8b189b932a5bbc91d94e228fd3500f75c516
defqd <DefQD@yandex.ru>
2022-05-06 21:10:22 +0300

    Add Solution

---
+++ CodeWarsTests/8 kyu/CountOddNumbersTests.cs
@@ -0,0 +1,16 @@
+﻿using NUnit.Framework;
+using CodeWars._8_kyu;
+
+namespace CodeWarsTests._8_kyu
+{
+    public class CountOddNumbersTests
+    {
+        [Test]
+        public void Test()
+        {
+            Assert.AreEqual(7, CountOddNumbers.OddCount(15));
+            Assert.AreEqual(7511, CountOddNumbers.OddCount(15023));
+        }
+    }
+}
+
