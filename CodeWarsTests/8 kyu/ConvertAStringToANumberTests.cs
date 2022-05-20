a6f22ceee54a143725b1e7acc592dc5996a3a4e2
defqd <DefQD@yandex.ru>
2022-05-20 23:32:35 +0300

    Added solution "Convert a String to a Number" 8 kyu

---
+++ CodeWarsTests/8 kyu/ConvertAStringToANumberTests.cs
@@ -0,0 +1,33 @@
+﻿using CodeWars._8_kyu;
+using NUnit.Framework;
+
+namespace CodeWarsTests._8_kyu
+{
+    [TestFixture]
+    public class ConvertAStringToANumberTests
+    {
+        [Test]
+        public void test1()
+        {
+            Assert.AreEqual(1234, ConvertAStringToANumber.StringToNumber("1234"));
+        }
+
+        [Test]
+        public void test2()
+        {
+            Assert.AreEqual(605, ConvertAStringToANumber.StringToNumber("605"));
+        }
+
+        [Test]
+        public void test3()
+        {
+            Assert.AreEqual(1405, ConvertAStringToANumber.StringToNumber("1405"));
+        }
+
+        [Test]
+        public void test4()
+        {
+            Assert.AreEqual(-7, ConvertAStringToANumber.StringToNumber("-7"));
+        }
+    }
+}
