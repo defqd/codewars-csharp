7e56d738739ea55011624e0758b3344032e12e09
defqd <DefQD@yandex.ru>
2022-05-21 17:40:18 +0300

    Added solution "Sum of differences in array" 8 kyu

---
+++ CodeWars/8 kyu/SumOfDifferencesInArray.cs
@@ -0,0 +1,36 @@
+﻿/*
+https://www.codewars.com/kata/5b73fe9fb3d9776fbf00009e
+
+8 kyu
+
+Your task is to sum the differences between consecutive pairs in the array in descending order.
+
+For example:
+
+SumOfDifferences(new int[] { 2, 1, 10 })
+Returns 9
+
+Descending order: [10, 2, 1]
+
+Sum: (10 - 2) + (2 - 1) = 8 + 1 = 9
+
+If the array is empty or the array has only one element the result should be 0 (Nothing in Haskell).
+*/
+namespace CodeWars._8_kyu
+{
+    public class SumOfDifferencesInArray
+    {
+        public static int SumOfDifferences(int[] arr)
+        {
+            //Array.Sort(arr);
+
+            //int sum = 0;
+            //for (int i = 1; i < arr.Length; i++)
+            //    sum += arr[i] - arr[i - 1];
+
+            //return sum;
+
+            return arr.Any() ? arr.Max() - arr.Min() : 0;
+        }
+    }
+}
---
+++ CodeWarsTests/8 kyu/SumOfDifferencesInArrayTests.cs
@@ -0,0 +1,26 @@
+﻿using CodeWars._8_kyu;
+using NUnit.Framework;
+
+namespace CodeWarsTests._8_kyu
+{
+    public class SumOfDifferencesInArrayTests
+    {
+        [Test]
+        public void SampleTest()
+        {
+            Assert.AreEqual(0, SumOfDifferencesInArray.SumOfDifferences(new int[] { }));
+            Assert.AreEqual(0, SumOfDifferencesInArray.SumOfDifferences(new int[] { 0 }));
+            Assert.AreEqual(0, SumOfDifferencesInArray.SumOfDifferences(new int[] { 1 }));
+            Assert.AreEqual(0, SumOfDifferencesInArray.SumOfDifferences(new int[] { -1 }));
+            Assert.AreEqual(34, SumOfDifferencesInArray.SumOfDifferences(new int[] { -17, 17 }));
+            Assert.AreEqual(9, SumOfDifferencesInArray.SumOfDifferences(new int[] { 2, 1, 10 }));
+            Assert.AreEqual(2, SumOfDifferencesInArray.SumOfDifferences(new int[] { -3, -2, -1 }));
+            Assert.AreEqual(0, SumOfDifferencesInArray.SumOfDifferences(new int[] { 1, 1, 1, 1, 1 }));
+            Assert.AreEqual(0, SumOfDifferencesInArray.SumOfDifferences(new int[] { 0, 0, 0, 0, 0 }));
+            Assert.AreEqual(4, SumOfDifferencesInArray.SumOfDifferences(new int[] { 1, 2, 3, 4, 5 }));
+            Assert.AreEqual(29, SumOfDifferencesInArray.SumOfDifferences(new int[] { 0, 29, 24, 17 }));
+            Assert.AreEqual(50, SumOfDifferencesInArray.SumOfDifferences(new int[] { 25, 24, -25, 2 }));
+            Assert.AreEqual(22, SumOfDifferencesInArray.SumOfDifferences(new int[] { -26, -4, -8, -8 }));
+        }
+    }
+}
