5f40e6ecd98d8f29d4ea80b7460180c9a86efd55
defqd <DefQD@yandex.ru>
2022-05-21 18:06:01 +0300

    Added solution "What is between?" 8 kyu

---
+++ CodeWars/8 kyu/WhatIsBetween.cs
@@ -0,0 +1,23 @@
+﻿/*
+https://www.codewars.com/kata/55ecd718f46fba02e5000029
+
+8 kyu
+
+Complete the function that takes two integers (a, b, where a < b) and return an array of all integers between the input parameters, including them.
+
+For example:
+
+a = 1
+b = 4
+--> [1, 2, 3, 4]
+*/
+namespace CodeWars._8_kyu
+{
+    public class WhatIsBetween
+    {
+        public static int[] Between(int a, int b)
+        {
+            return Enumerable.Range(a, b + 1 - a).ToArray();
+        }
+    }
+}
---
+++ CodeWarsTests/8 kyu/WhatIsBetweenTests.cs
@@ -0,0 +1,17 @@
+﻿using CodeWars._8_kyu;
+using NUnit.Framework;
+
+namespace CodeWarsTests._8_kyu
+{
+    [TestFixture]
+    public class WhatIsBetweenTests
+    {
+        [Test]
+        public void BasicTests()
+        {
+            Assert.AreEqual(new int[] { 0, 1, 2, 3 }, WhatIsBetween.Between(0, 3));
+            Assert.AreEqual(new int[] { -2, -1, 0, 1, 2 }, WhatIsBetween.Between(-2, 2));
+            Assert.AreEqual(new int[] { -10, -9, -8, -7, -6, -5, -4, -3, -2, -1, 0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 }, WhatIsBetween.Between(-10, 10));
+        }
+    }
+}
