53580cacfb7d9c32c4c4cdb420f58c0d5197aa72
defqd <DefQD@yandex.ru>
2022-06-11 19:36:29 +0300

    Added solution "Deodorant Evaporator" 7 kyu

---
+++ CodeWarsTests/7 kyu/DeodorantEvaporatorTests.cs
@@ -0,0 +1,18 @@
+﻿using CodeWars._7_kyu;
+using NUnit.Framework;
+
+namespace CodeWarsTests._7_kyu
+{
+
+    [TestFixture]
+    public class DeodorantEvaporatorTests
+    {
+
+        [Test]
+        public void Test1()
+        {
+            Assert.AreEqual(22, DeodorantEvaporator.Evaporator(10, 10, 10));
+            Assert.AreEqual(29, DeodorantEvaporator.Evaporator(10, 10, 5));
+        }
+    }
+}
