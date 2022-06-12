e87768a05d9665ae642f80ea7d54185a9c87fde4
defqd <DefQD@yandex.ru>
2022-06-12 18:30:39 +0300

    Added solution "Double Char" 8 kyu

---
+++ CodeWars/8 kyu/KataDoubleChar.cs
@@ -0,0 +1,42 @@
+﻿/*
+https://www.codewars.com/kata/56b1f01c247c01db92000076
+
+8 kyu 
+
+Given a string, you have to return a string in which each character (case-sensitive) is repeated once.
+
+Examples (Input -> Output):
+* "String"      -> "SSttrriinngg"
+* "Hello World" -> "HHeelllloo  WWoorrlldd"
+* "1234!_ "     -> "11223344!!__  "
+Good Luck!
+*/
+
+using System.Text.RegularExpressions;
+
+namespace CodeWars._8_kyu
+{
+    public class KataDoubleChar
+    {
+        public static string DoubleChar(string s)
+        {
+            return Regex.Replace(s, ".", "$&$&");
+        }
+
+        //public static string DoubleChar(string s)
+        //{
+        //    return string.Join("", s.Select(x => "" + x + x));
+        //}
+
+        //public static string DoubleChar(string s)
+        //{
+        //    string result = "";
+        //    foreach (var character in s)
+        //    {
+        //        result += character;
+        //        result += character;
+        //    }
+        //    return result;
+        //}
+    }
+}
---
+++ CodeWarsTests/8 kyu/KataDoubleCharTests.cs
@@ -0,0 +1,21 @@
+﻿using CodeWars._8_kyu;
+using NUnit.Framework;
+
+namespace CodeWarsTests._8_kyu
+{
+    [TestFixture]
+    public class KataDoubleCharTests
+    {
+        [Test]
+        [TestCase("abcd", ExpectedResult = "aabbccdd")]
+        [TestCase("Adidas", ExpectedResult = "AAddiiddaass")]
+        [TestCase("1337", ExpectedResult = "11333377")]
+        [TestCase("illuminati", ExpectedResult = "iilllluummiinnaattii")]
+        [TestCase("123456", ExpectedResult = "112233445566")]
+        [TestCase("%^&*(", ExpectedResult = "%%^^&&**((")]
+        public static string FixedTest(string s)
+        {
+            return KataDoubleChar.DoubleChar(s);
+        }
+    }
+}
