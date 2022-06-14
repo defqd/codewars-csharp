f00c10001a0da75972fe37374d2da81ac255564f
defqd <DefQD@yandex.ru>
2022-06-14 21:43:15 +0300

    Added solution "FIXME: Replace all dots" 8 kyu

---
+++ CodeWars/8 kyu/ReplaceAllDots.cs
@@ -0,0 +1,27 @@
+﻿/*
+https://www.codewars.com/kata/596c6eb85b0f515834000049
+
+8 kyu
+ 
+The code provided is supposed replace all the dots . in the specified String str with dashes -
+
+But it's not working properly.
+
+Task
+Fix the bug so we can all go home early.
+
+Notes
+String str will never be null.
+*/
+using System.Text.RegularExpressions;
+
+namespace CodeWars._8_kyu
+{
+    public class ReplaceAllDots
+    {
+        public static string ReplaceDots(string str)
+        {
+            return new Regex("\\.").Replace(str, "-");
+        }
+    }
+}
---
+++ CodeWarsTests/8 kyu/ReplaceAllDotsTests.cs
@@ -0,0 +1,15 @@
+﻿using CodeWars._8_kyu;
+using NUnit.Framework;
+
+namespace CodeWarsTests._8_kyu
+{
+    [TestFixture]
+    public class ReplaceAllDotsTests
+    {
+        [Test]
+        public void SampleTest()
+        {
+            Assert.AreEqual("one-two-three", ReplaceAllDots.ReplaceDots("one.two.three"));
+        }
+    }
+}
