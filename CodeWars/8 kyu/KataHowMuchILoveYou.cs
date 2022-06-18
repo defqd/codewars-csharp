ffa8590e948cab9e660d1556fcb027c6c5128970
defqd <DefQD@yandex.ru>
2022-06-18 17:14:33 +0300

    Added solution "I love you, a little , a lot, passionately ... not at all" 8 kyu

---
+++ CodeWars/8 kyu/KataHowMuchILoveYou.cs
@@ -0,0 +1,62 @@
+﻿/*
+https://www.codewars.com/kata/57f24e6a18e9fad8eb000296
+
+8 kuy
+
+Who remembers back to their time in the schoolyard, when girls would take a flower and tear its petals, saying each of the following phrases each time a petal was
+torn:
+
+I love you
+a little
+a lot
+passionately
+madly
+not at all
+When the last petal was torn there were cries of excitement, dreams, surging thoughts and emotions.
+
+Your goal in this kata is to determine which phrase the girls would say for a flower of a given number of petals, where nb_petals > 0.
+*/
+namespace CodeWars._8_kyu
+{
+    public class KataHowMuchILoveYou
+    {
+        public static string HowMuchILoveYou(int nb_petals)
+        {
+
+            return new string[] { "I love you", "a little", "a lot", "passionately", "madly", "not at all" }[(nb_petals - 1) % 6];
+
+            //switch (nb_petals % 6)
+            //{
+            //    case 1:
+            //        return "I love you";
+            //    case 2:
+            //        return "a little";
+            //    case 3:
+            //        return "a lot";
+            //    case 4:
+            //        return "passionately";
+            //    case 5:
+            //        return "madly";
+            //    default: return "not at all";
+            //}
+
+            //switch (Math.Abs(nb_petals - 6) <= 6 ? Math.Abs(nb_petals - 6) : nb_petals - (nb_petals / 6) * 6)
+            //{
+            //    case 1:
+            //        return "I love you";
+            //    case 2:
+            //        return "a little";
+            //    case 3:
+            //        return "a lot";
+            //    case 4:
+            //        return "passionately";
+            //    case 5:
+            //        return "madly";
+            //    case 6:
+            //    case 0:
+            //        return "not at all";
+            //}
+            //return "";
+        }
+    }
+}
---
+++ CodeWarsTests/8 kyu/KataHowMuchILoveYouTests.cs
@@ -0,0 +1,21 @@
+﻿using CodeWars._8_kyu;
+using NUnit.Framework;
+
+namespace CodeWarsTests._8_kyu
+{
+    [TestFixture]
+    public class KataHowMuchILoveYouTests
+    {
+        [Test]
+        public void SampleTests()
+        {
+            Assert.AreEqual("I love you", KataHowMuchILoveYou.HowMuchILoveYou(7));
+            Assert.AreEqual("a lot", KataHowMuchILoveYou.HowMuchILoveYou(3));
+            Assert.AreEqual("not at all", KataHowMuchILoveYou.HowMuchILoveYou(6));
+            Assert.AreEqual("not at all", KataHowMuchILoveYou.HowMuchILoveYou(12));
+            Assert.AreEqual("not at all", KataHowMuchILoveYou.HowMuchILoveYou(18));
+            Assert.AreEqual("passionately", KataHowMuchILoveYou.HowMuchILoveYou(16));
+            Assert.AreEqual("madly", KataHowMuchILoveYou.HowMuchILoveYou(47));
+        }
+    }
+}
