6e7ca3db8adecb20cedfdd4b3aeec27fe6cfb2f2
defqd <DefQD@yandex.ru>
2022-08-08 19:28:01 +0300

    Added solution "Is this my tail?" 8 kyu

---
+++ CodeWarsTests/8 kyu/IsThisMyTailTests.cs
@@ -0,0 +1,28 @@
+﻿using CodeWars._8_kyu;
+using NUnit.Framework;
+using System.Collections.Generic;
+
+namespace CodeWarsTests._8_kyu
+{
+    [TestFixture]
+    public class IsThisMyTailTests
+    {
+        
+            private static IEnumerable<TestCaseData> testCases
+            {
+                get
+                {
+                    yield return new TestCaseData("Fox", "x").Returns(true);
+                    yield return new TestCaseData("Rhino", "o").Returns(true);
+                    yield return new TestCaseData("Meerkat", "t").Returns(true);
+                    yield return new TestCaseData("Emu", "t").Returns(false);
+                    yield return new TestCaseData("Badger", "s").Returns(false);
+                    yield return new TestCaseData("Giraffe", "d").Returns(false);
+                }
+            }
+
+            [Test, TestCaseSource("testCases")]
+            public bool Test(string body, string tail) => IsThisMyTail.CorrectTail(body, tail);
+        
+    }
+}
