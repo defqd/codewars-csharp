afab6c13810e3896cd944a51253a3450d291ca0b
defqd <DefQD@yandex.ru>
2022-08-08 20:14:01 +0300

    Added solution "All unique" 8 kyu

---
+++ CodeWars/7 kyu/AllUnique.cs
@@ -0,0 +1,32 @@
+﻿/*
+
+7 kuy
+
+https://www.codewars.com/kata/553e8b195b853c6db4000048
+
+Write a program to determine if a string contains only unique characters. Return true if it does and false otherwise.
+
+The string may contain any of the 128 ASCII characters. Characters are case-sensitive, e.g. 'a' and 'A' are considered different characters.
+
+*/
+namespace CodeWars._7_kyu
+{
+    public class AllUnique
+    {
+        public static bool HasUniqueChars(string str)
+        {
+
+            //return new String(str.Distinct().ToArray()).Length < str.Length ? false : true;
+
+            //return str.Distinct().Count() < str.Length ? false : true;
+
+            //return new HashSet<char>(str).Count < str.Length ? false : true;
+
+            //return str.Distinct().SequenceEqual(str);
+
+            //return str.Distinct().Count() == str.Length;
+
+            return string.Concat(str.Distinct()) == str;
+        }
+    }
+}
---
+++ CodeWarsTests/7 kyu/AllUniqueTests.cs
@@ -0,0 +1,33 @@
+﻿using CodeWars._7_kyu;
+using NUnit.Framework;
+
+namespace CodeWarsTests._7_kyu
+{
+    [TestFixture]
+    public class AllUniqueTests
+    {
+        [Test]
+        public void TestHasUniqueChars1()
+        {
+           Assert.IsTrue(AllUnique.HasUniqueChars("abcdef"));
+        }
+
+        [Test]
+        public void TestHasUniqueChars2()
+        {
+            Assert.False(AllUnique.HasUniqueChars("++-"));
+        }
+
+        [Test]
+        public void TestHasUniqueChars3()
+        {
+            Assert.False(AllUnique.HasUniqueChars("  nAa"));
+        }
+
+        [Test]
+        public void TestHasUniqueChars4()
+        {
+            Assert.False(AllUnique.HasUniqueChars("aba"));
+        }
+    }
+}
