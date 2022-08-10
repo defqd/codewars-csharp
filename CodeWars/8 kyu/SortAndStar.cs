082125905e3a87cbe8967ee05a0a19448ef5eae1
defqd <DefQD@yandex.ru>
2022-08-10 18:43:15 +0300

    Added solution "Sort and Star" 8 kyu

---
+++ CodeWars/8 kyu/SortAndStar.cs
@@ -0,0 +1,29 @@
+﻿/*
+
+https://www.codewars.com/kata/57cfdf34902f6ba3d300001e
+
+8 kuy
+
+You will be given a list of strings. You must sort it alphabetically (case-sensitive, and based on the ASCII values of the chars) and then return the first value.
+
+The returned value must be a string, and have "***" between each of its letters.
+
+You should not remove or add elements from/to the array.
+
+*/
+namespace CodeWars._8_kyu
+{
+    public class SortAndStar
+    {
+        public static string TwoSort(string[] s)
+        {
+            Array.Sort(s, StringComparer.Ordinal);
+            char[] c = s[0].ToCharArray();
+            return string.Join("***", c);
+
+            //return string.Concat(s.OrderBy(s => s, StringComparer.Ordinal).First().Select(x => x + "***")).TrimEnd('*');
+
+            //return string.Join("***", s.OrderBy(a => a, StringComparer.Ordinal).First().ToArray());
+        }
+    }
+}
---
+++ CodeWarsTests/8 kyu/SortAndStarTests.cs
@@ -0,0 +1,18 @@
+﻿using CodeWars._8_kyu;
+using NUnit.Framework;
+
+namespace CodeWarsTests._8_kyu
+{
+    [TestFixture]
+    public class SortAndStarTests
+    {
+        [Test]
+        public void BasicTests()
+        {
+            Assert.AreEqual("b***i***t***c***o***i***n", SortAndStar.TwoSort(new[] { "bitcoin", "take", "over", "the", "world", "maybe", "who", "knows", "perhaps" }));
+            Assert.AreEqual("a***r***e", SortAndStar.TwoSort(new[] { "turns", "out", "random", "test", "cases", "are", "easier", "than", "writing", "out", "basic", "ones" }));
+            Assert.AreEqual("L***e***t***s", SortAndStar.TwoSort(new[] { "writing","world", "maybe","Lets","all" }));
+            Assert.AreEqual("B***T***C", SortAndStar.TwoSort(new[] { "writing","world", "maybe","Lets","all", "Bitcoin", "BTC" }));
+        }
+    }
+}
