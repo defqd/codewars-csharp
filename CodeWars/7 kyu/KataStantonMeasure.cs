3263af3b69064949f91262ed03db4cff1c87d9ec
defqd <DefQD@yandex.ru>
2022-09-05 22:10:19 +0300

    Added solution "Stanton measure" 7 kyu

---
+++ CodeWars/7 kyu/KataStantonMeasure.cs
@@ -0,0 +1,45 @@
+﻿/*
+
+https://www.codewars.com/kata/59a1cdde9f922b83ee00003b/csharp
+
+7 kuy
+
+The Stanton measure of an array is computed as follows: count the number of occurences for value 1 in the array. Let this count be n. The Stanton measure is the number of times that n appears in the array.
+
+Write a function which takes an integer array and returns its Stanton measure.
+
+Examples
+The Stanton measure of [1, 4, 3, 2, 1, 2, 3, 2] is 3, because 1 occurs 2 times in the array and 2 occurs 3 times.
+
+The Stanton measure of [1, 4, 1, 2, 11, 2, 3, 1] is 1, because 1 occurs 3 times in the array and 3 occurs 1 time.
+
+*/
+
+namespace CodeWars._7_kyu
+{
+    public class KataStantonMeasure
+    {
+        public static int StantonMeasure(int[] arr)
+        {
+            int n = 0;
+            int count = 0;
+            for (int i = 0; i < arr.Length; i++)
+            {
+                if (arr[i] == 1)
+                {
+                    n++;
+                }
+            }
+            for (int j = 0; j < arr.Length; j++)
+            {
+                if (arr[j] == n)
+                {
+                    count++;
+                }
+            }
+            
+            return count;
+        }
+    }
+    
+}
---
+++ CodeWarsTests/7 kyu/KataStantonMeasureTests.cs
@@ -0,0 +1,16 @@
+﻿using CodeWars._7_kyu;
+using NUnit.Framework;
+
+namespace CodeWarsTests._7_kyu
+{
+    [TestFixture]
+    public class KataStantonMeasureTests
+    {
+        [Test]
+        public void SampleTest()
+        {
+            Assert.AreEqual(3, KataStantonMeasure.StantonMeasure(new int[] { 1, 4, 3, 2, 1, 2, 3, 2 }));
+            Assert.AreEqual(1, KataStantonMeasure.StantonMeasure(new int[] { 1, 4, 1, 2, 11, 2, 3, 1 }));
+        }
+    }
+}
