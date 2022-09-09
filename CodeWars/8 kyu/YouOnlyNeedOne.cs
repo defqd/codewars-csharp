face7435ce3adc0dd902b5a316f4ff33da7f5a77
defqd <DefQD@yandex.ru>
2022-09-09 23:25:50 +0300

    Added solution "You only need one - Beginner" 8 kyu

---
+++ CodeWars/8 kyu/YouOnlyNeedOne.cs
@@ -0,0 +1,26 @@
+﻿/*
+
+https://www.codewars.com/kata/57cc975ed542d3148f00015b
+
+8 kyu
+
+You will be given an array a and a value x. All you need to do is check whether the provided array contains the value.
+
+Array can contain numbers or strings. X can be either.
+
+Return true if the array contains the value, false if not.
+ 
+*/
+namespace CodeWars._8_kyu
+{
+    public class YouOnlyNeedOne
+    {
+        public static bool Check(object[] a, object x)
+        {
+            return a.Contains(x);
+
+            //var result = Array.Find(a, i => i.Equals(x));
+            //return result is null ? false: true;
+        }
+    }
+}
---
+++ CodeWarsTests/8 kyu/YouOnlyNeedOneTests.cs
@@ -0,0 +1,19 @@
+﻿using CodeWars._8_kyu;
+using NUnit.Framework;
+
+namespace CodeWarsTests._8_kyu
+{
+    [TestFixture]
+    public class YouOnlyNeedOneTests
+    {
+        [Test]
+        public void BasicTests()
+        {
+            Assert.AreEqual(true, YouOnlyNeedOne.Check(new object[] { 66, 101 }, 66));
+            Assert.AreEqual(true, YouOnlyNeedOne.Check(new object[] { 80, 117, 115, 104, 45, 85, 112, 115 }, 45));
+
+            Assert.AreEqual(true, YouOnlyNeedOne.Check(new object[] { 't', 'e', 's', 't' }, 'e'));
+            Assert.AreEqual(false, YouOnlyNeedOne.Check(new object[] { "what", "a", "great", "kata" }, "kat"));
+        }
+    }
+}
