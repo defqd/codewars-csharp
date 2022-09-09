65830d523f58e2d1868317b27a8c614e6e477f8d
defqd <DefQD@yandex.ru>
2022-09-09 22:16:46 +0300

    Added solution "Grasshopper - Terminal game move function" 8 kyu

---
+++ CodeWarsTests/8 kyu/GrasshopperTests.cs
@@ -0,0 +1,16 @@
+﻿using CodeWars._8_kyu;
+using NUnit.Framework;
+
+namespace CodeWarsTests._8_kyu
+{
+    [TestFixture]
+    public class GrasshopperTests
+    {
+        [Test]
+        public void Test1()
+        {
+            Assert.That(Grasshopper.Move(0, 4), Is.EqualTo(8));
+            Assert.That(Grasshopper.Move(3, 6), Is.EqualTo(15));
+        }
+    }
+}
