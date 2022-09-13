632b9e4deb6169a53773f8c63aa33c41c9f122f3
defqd <DefQD@yandex.ru>
2022-09-13 22:06:35 +0300

    Added solution "N-th Power" 8 kyu

---
+++ CodeWars/8 kyu/N-thPower.cs
@@ -0,0 +1,43 @@
+﻿/*
+
+https://www.codewars.com/kata/57d814e4950d8489720008db
+
+8 kyu
+
+N-th PoweThis kata is from check py.checkio.org
+
+You are given an array with positive numbers and a non-negative number N. You should find the N-th power of the element in the array with the index N. If N is outside of the array, then return -1. Don't forget that the first element has the index 0.
+
+Let's look at a few examples:
+
+array = [1, 2, 3, 4] and N = 2, then the result is 3^2 == 9;
+array = [1, 2, 3] and N = 3, but N is outside of the array, so the result is -1.
+ 
+*/
+namespace CodeWars._8_kyu
+{
+    public class N_thPower
+    {
+        public static double Index(int[] array, int n)
+        {
+            try
+            {
+                return Math.Pow(array[n], n);
+            }
+            catch
+            {
+                return -1;
+            }
+        }
+
+        //public static double Index(int[] array, int n) => n < array.Length ? Math.Pow(array[n], n) : -1;
+
+        //public static double Index(int[] array, int n)
+        //{
+        //    if (array.Length - 1 >= n)
+        //        return array.Contains(array[n]) ? Math.Pow(array[n], n) : -1;
+
+        //    else return -1;
+        //}
+    }
+}
---
+++ CodeWarsTests/8 kyu/N_thPowerTests.cs
@@ -0,0 +1,22 @@
+﻿using CodeWars._8_kyu;
+using NUnit.Framework;
+
+namespace CodeWarsTests._8_kyu
+{
+    [TestFixture]
+    public class N_thPowerTests
+    {
+        [Test]
+        public void SampleTest()
+        {
+            Assert.AreEqual(9, N_thPower.Index(new int[] { 1, 2, 3, 4 }, 2));
+            Assert.AreEqual(1000000, N_thPower.Index(new int[] { 1, 3, 10, 100 }, 3));
+            Assert.AreEqual(1, N_thPower.Index(new int[] { 6, 2, 10, 200 }, 0));
+            Assert.AreEqual(1, N_thPower.Index(new int[] { 0, 2, 10, 200 }, 0));
+            Assert.AreEqual(1000, N_thPower.Index(new int[] { 29, 82, 45, 10}, 3));
+            Assert.AreEqual(70, N_thPower.Index(new int[] { 23, 70, 48, 27, 63}, 1));          
+            Assert.AreEqual(-1, N_thPower.Index(new int[] { 6, 31}, 3));        
+            Assert.AreEqual(-1, N_thPower.Index(new int[] { 59, 35, 61, 97, 47, 28, 17}, 9));        
+        }
+    }
+}
