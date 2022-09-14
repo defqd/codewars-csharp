a8315c4f9edf95d3deabc24f59e91199fcd712c5
defqd <DefQD@yandex.ru>
2022-09-14 22:23:53 +0300

    Added solution "Name On Billboard" 8 kyu

---
+++ CodeWarsTests/8 kyu/NameOnBillboardTests.cs
@@ -0,0 +1,28 @@
+﻿using CodeWars._8_kyu;
+using NUnit.Framework;
+
+namespace CodeWarsTests._8_kyu
+{
+    [TestFixture]
+    public class NameOnBillboardTests
+    {
+        [Test]
+        public void SampleTests()
+        {
+            Assertion(600, "Jeong-Ho Aristotelis");
+            Assertion(40, "CODEWARS", 5);
+        }
+
+        private static void Assertion(double expected, string name, double price = -1) =>
+          Assert.AreEqual(
+              expected,
+              price == -1 ? NameOnBillboard.Billboard(name) : NameOnBillboard.Billboard(name, price),
+
+              0.000001,
+
+              $"\n  Name: \"{name}\"\n" +
+                $"  Price: {(price == -1 ? 30 : price)}\n\n"
+
+          );
+    }
+}
