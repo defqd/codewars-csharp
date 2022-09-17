ffd1c4cd190afd83a79a2d81f1f0279cd75bf7a1
defqd <DefQD@yandex.ru>
2022-09-17 20:39:59 +0300

    Added solution "Abbreviate a Two Word Name" 8 kyu

---
+++ CodeWars/8 kyu/AbbreviateATwoWordName.cs
@@ -0,0 +1,29 @@
+﻿/*
+ 
+https://www.codewars.com/kata/57eadb7ecd143f4c9c0000a3
+
+8 kyu
+
+Write a function to convert a name into initials. This kata strictly takes two words with one space in between them.
+
+The output should be two capital letters with a dot separating them.
+
+It should look like this:
+
+Sam Harris => S.H
+
+patrick feeney => P.F
+
+ */
+namespace CodeWars._8_kyu
+{
+    public class AbbreviateATwoWordName
+    {
+        public static string AbbrevName(string name)
+        {
+            return String.Join(".", name.Split(" ").Select(x => char.ToUpper(x[0])));
+
+            //return $"{name[0]}.{name[name.IndexOf(' ') + 1]}".ToUpper();
+        }
+    }
+}
---
+++ CodeWarsTests/8 kyu/AbbreviateATwoWordNameTests.cs
@@ -0,0 +1,18 @@
+﻿using CodeWars._8_kyu;
+using NUnit.Framework;
+
+namespace CodeWarsTests._8_kyu
+{
+    public class AbbreviateATwoWordNameTests
+    {
+        [Test]
+        public void BasicTests()
+        {
+            Assert.AreEqual("S.H", AbbreviateATwoWordName.AbbrevName("Sam Harris"));
+            Assert.AreEqual("P.F", AbbreviateATwoWordName.AbbrevName("Patrick Feenan"));
+            Assert.AreEqual("E.C", AbbreviateATwoWordName.AbbrevName("Evan Cole"));
+            Assert.AreEqual("P.F", AbbreviateATwoWordName.AbbrevName("P Favuzzi"));
+            Assert.AreEqual("D.M", AbbreviateATwoWordName.AbbrevName("David Mendieta"));
+        }
+    }
+}
