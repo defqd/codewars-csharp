a13acac44c283845b655b7cc0da3d5fa6f53b1c9
defqd <DefQD@yandex.ru>
2022-09-17 22:00:43 +0300

    Added solution "Switcheroo" 7 kyu

---
+++ CodeWars/7 kyu/SwitcHeroo.cs
@@ -0,0 +1,45 @@
+﻿/*
+ 
+https://www.codewars.com/kata/57f759bb664021a30300007d
+
+7 kyu
+
+Given a string made up of letters a, b, and/or c, switch the position of letters a and b (change a to b and vice versa). Leave any incidence of c untouched.
+
+Example:
+
+'acb' --> 'bca'
+'aabacbaa' --> 'bbabcabb'
+
+*/
+
+using System.Text.RegularExpressions;
+
+namespace CodeWars._7_kyu
+{
+    public class SwitcHeroo
+    {
+        public static string Switcheroo(string x)
+        {
+            return x.Replace("a", "d").Replace("b", "a").Replace("d", "b");
+
+            //return Regex.Replace(x, "[ab]", m => m.Value == "a" ? "b" : "a");
+
+            //var result = "";
+            //foreach (var item in x)
+            //{
+            //    if (item == 'a')
+            //        result += 'b';
+
+            //    else if (item == 'b')
+            //        result += 'a';
+
+            //    else
+            //        result += item;
+            //}
+            //return result;
+
+            //return new string(x.Select(i => i == 'a' ? i = 'b' : (i == 'b' ? 'a' : i)).ToArray());
+        }
+    }
+}
---
+++ CodeWarsTests/7 kyu/SwitcHerooTests.cs
@@ -0,0 +1,16 @@
+﻿using CodeWars._7_kyu;
+using NUnit.Framework;
+
+namespace CodeWarsTests._7_kyu
+{
+    public class SwitcHerooTests
+    {
+        [Test]
+        public void BasicTests()
+        {
+            Assert.AreEqual("bac", SwitcHeroo.Switcheroo("abc"));
+            Assert.AreEqual("bbbacccabbb", SwitcHeroo.Switcheroo("aaabcccbaaa"));
+            Assert.AreEqual("ccccc", SwitcHeroo.Switcheroo("ccccc"));
+        }
+    }
+}
