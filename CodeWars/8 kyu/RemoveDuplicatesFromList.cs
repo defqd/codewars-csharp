5d98bf16a6823ceb3a6a55dd592a66d512fd979a
defqd <DefQD@yandex.ru>
2022-09-18 20:15:44 +0300

    Added solution "Remove duplicates from list" 8 kyu

---
+++ CodeWars/8 kyu/RemoveDuplicatesFromList.cs
@@ -0,0 +1,22 @@
+﻿/*
+
+https://www.codewars.com/kata/57a5b0dfcf1fa526bb000118
+
+8 kyu
+
+Define a function that removes duplicates from an array of numbers and returns it as a result.
+
+The order of the sequence has to stay the same.
+ 
+*/
+
+namespace CodeWars._8_kyu
+{
+    public class RemoveDuplicatesFromList
+    {
+        public static int[] Distinct(int[] a)
+        {
+            return a.Distinct().ToArray();
+        }
+    }
+}
---
+++ CodeWarsTests/8 kyu/RemoveDuplicatesFromListTests.cs
@@ -0,0 +1,16 @@
+﻿using CodeWars._8_kyu;
+using NUnit.Framework;
+
+namespace CodeWarsTests._8_kyu
+{
+    public class RemoveDuplicatesFromListTests
+    {
+        [Test]
+        public void MyTest()
+        {
+            Assert.AreEqual(new int[] { 1 }, RemoveDuplicatesFromList.Distinct(new int[] { 1 }));
+            Assert.AreEqual(new int[] { 1, 2 }, RemoveDuplicatesFromList.Distinct(new int[] { 1, 2 }));
+            Assert.AreEqual(new int[] { 1, 2 }, RemoveDuplicatesFromList.Distinct(new int[] { 1, 1, 2 }));
+        }
+    }
+}
