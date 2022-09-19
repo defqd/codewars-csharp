ac759d364e56f47c6f9030cbad850f3f09e74ae6
defqd <DefQD@yandex.ru>
2022-09-19 20:15:05 +0300

    Added solution "Number of People in the Bus" 7 kyu

---
+++ CodeWarsTests/7 kyu/NumberOfPeopleInTheBusTests.cs
@@ -0,0 +1,28 @@
+﻿using CodeWars._7_kyu;
+using NUnit.Framework;
+using System.Collections.Generic;
+
+namespace CodeWarsTests._7_kyu
+{
+    public class NumberOfPeopleInTheBusTests
+    {
+        [Test]
+        public void FirstTest()
+        {
+            List<int[]> peopleList = new List<int[]>() { new[] { 10, 0 }, new[] { 3, 5 }, new[] { 5, 8 } };
+            Assert.AreEqual(5, NumberOfPeopleInTheBus.Number(peopleList));
+        }
+        [Test]
+        public void SecondTest()
+        {
+            List<int[]> peopleList = new List<int[]>() { new[] { 3, 0 }, new[] { 9, 1 }, new[] { 4, 10 }, new[] { 12, 2 }, new[] { 6, 1 }, new[] { 7, 10 } };
+            Assert.AreEqual(17, NumberOfPeopleInTheBus.Number(peopleList));
+        }
+        [Test]
+        public void ThirdTest()
+        {
+            List<int[]> peopleList = new List<int[]>() { new[] { 3, 0 }, new[] { 9, 1 }, new[] { 4, 8 }, new[] { 12, 2 }, new[] { 6, 1 }, new[] { 7, 8 } };
+            Assert.AreEqual(21, NumberOfPeopleInTheBus.Number(peopleList));
+        }
+    }
+}
