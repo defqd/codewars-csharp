a5ae3b8cdd7d90e68b59c2d58ef21a1eb9540c05
defqd <DefQD@yandex.ru>
2022-09-21 21:24:05 +0300

    Added solution "Is the string uppercase?" 8 kyu

---
+++ CodeWarsTests/8 kyu/IsTheStringUppercaseTests.cs
@@ -0,0 +1,20 @@
+﻿using CodeWars._8_kyu;
+using NUnit.Framework;
+
+namespace CodeWarsTests._8_kyu
+{
+    public class IsTheStringUppercaseTests
+    {
+        [Test]
+        [TestCase("c", ExpectedResult = false)]
+        [TestCase("C", ExpectedResult = true)]
+        [TestCase("hello I AM DONALD", ExpectedResult = false)]
+        [TestCase("HELLO I AM DONALD", ExpectedResult = true)]
+        [TestCase("ACSKLDFJSgSKLDFJSKLDFJ", ExpectedResult = false)]
+        [TestCase("ACSKLDFJSGSKLDFJSKLDFJ", ExpectedResult = true)]
+        public static bool FixedTest(string text)
+        {
+            return text.IsUpperCase();
+        }
+    }
+}
