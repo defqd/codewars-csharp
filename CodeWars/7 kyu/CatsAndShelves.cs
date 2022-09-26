2ef1ccf241d3f440877534bd33e2d83ce27d2fe8
defqd <DefQD@yandex.ru>
2022-09-26 20:55:39 +0300

    Added solution "Cats and shelves" 7 kyu

---
+++ CodeWars/7 kyu/CatsAndShelves.cs
@@ -0,0 +1,59 @@
+﻿/*
+
+https://www.codewars.com/kata/62c93765cef6f10030dfa92b
+
+7 kyu
+
+Description
+An infinite number of shelves are arranged one above the other in a staggered fashion.
+The cat can jump up to 3 shelves at the same time: from shelf 1 to shelf 2 or 4 (the cat cannot climb on the shelf directly above its head), according to the illustration:
+
+                 ┌────────┐
+                 │-6------│
+                 └────────┘
+┌────────┐       
+│------5-│        
+└────────┘  ┌─────► OK!
+            │    ┌────────┐
+            │    │-4------│
+            │    └────────┘
+┌────────┐  │
+│------3-│  │     
+BANG!────┘  ├─────► OK! 
+  ▲  |\_/|  │    ┌────────┐
+  │ ("^-^)  │    │-2------│
+  │ )   (   │    └────────┘
+┌─┴─┴───┴┬──┘
+│------1-│
+└────────┘
+Input
+Start and finish shelf numbers (always positive integers, finish no smaller than start)
+
+Task
+Find the minimum number of jumps to go from start to finish
+
+Example
+Start 1, finish 5, then answer is 2 (1 => 4 => 5 or 1 => 2 => 5)
+ 
+*/
+namespace CodeWars._7_kyu
+{
+    public class CatsAndShelves
+    {
+        public static int Cats(int start, int finish)
+        {
+            return (finish - start) / 3 + (finish - start) % 3;
+
+            //int result = 0;
+            //while (start < finish)
+            //{
+            //    result++;
+            //    if (start + 3 <= finish)
+            //        start += 3;
+            //    else
+            //        start += 1;
+            //}
+            //return result;
+        }
+    }
+}
---
+++ CodeWarsTests/7 kyu/CatsAndShelvesTests.cs
@@ -0,0 +1,34 @@
+﻿using CodeWars._7_kyu;
+using NUnit.Framework;
+
+namespace CodeWarsTests._7_kyu
+{
+    [TestFixture]
+    public class CatsAndShelvesTests
+    {
+        private static void testing(int actual, int expected)
+        {
+            Assert.AreEqual(expected, actual);
+        }
+        [Test]
+        public static void FrezzyCat()
+        {
+            testing(CatsAndShelves.Cats(1, 1), 0);
+        }
+        [Test]
+        public static void Mew()
+        {
+            testing(CatsAndShelves.Cats(1, 5), 2);
+        }
+        [Test]
+        public static void OneMoreMew()
+        {
+            testing(CatsAndShelves.Cats(2, 5), 1);
+        }
+        [Test]
+        public static void WoolenBag()
+        {
+            testing(CatsAndShelves.Cats(2, 4), 2);
+        }
+    }
+}
