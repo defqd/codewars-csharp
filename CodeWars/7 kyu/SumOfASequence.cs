ad5137b4de95f9f66c197b3577a1d1b096db625c
defqd <DefQD@yandex.ru>
2022-10-23 22:45:31 +0300

    Added solution "Sum of a sequence" 7 kyu

---
+++ CodeWars/7 kyu/SumOfASequence.cs
@@ -0,0 +1,35 @@
+﻿/*
+
+https://www.codewars.com/kata/586f6741c66d18c22800010a
+
+7 kyu
+
+Your task is to make function, which returns the sum of a sequence of integers.
+
+The sequence is defined by 3 non-negative values: begin, end, step (inclusive).
+
+If begin value is greater than the end, function should returns 0
+
+Examples
+
+2,2,2 --> 2
+2,6,2 --> 12 (2 + 4 + 6)
+1,5,1 --> 15 (1 + 2 + 3 + 4 + 5)
+1,5,3  --> 5 (1 + 4)
+*/
+
+namespace CodeWars._7_kyu
+{
+    public class SumOfASequence
+    {
+        public static int SequenceSum(int start, int end, int step)
+        {
+            var result = new List<int>();
+
+            for (int i = start; i <= end; i += step)
+                result.Add(i);
+
+            return result.Sum();
+        }
+    }
+}
---
+++ CodeWarsTests/7 kyu/SumOfASequenceTests.cs
@@ -0,0 +1,27 @@
+﻿using CodeWars._7_kyu;
+using NUnit.Framework;
+
+namespace CodeWarsTests._7_kyu
+{
+    public class SumOfASequenceTests
+    {
+        [TestFixture]
+        public class SolutionTest
+        {
+            [Test]
+            public void BasicTest()
+            {
+                Assert.AreEqual(12, SumOfASequence.SequenceSum(2, 6, 2));
+                Assert.AreEqual(15, SumOfASequence.SequenceSum(1, 5, 1));
+                Assert.AreEqual(5, SumOfASequence.SequenceSum(1, 5, 3));
+                Assert.AreEqual(45, SumOfASequence.SequenceSum(0, 15, 3));
+                Assert.AreEqual(0, SumOfASequence.SequenceSum(16, 15, 3));
+                Assert.AreEqual(26, SumOfASequence.SequenceSum(2, 24, 22));
+                Assert.AreEqual(2, SumOfASequence.SequenceSum(2, 2, 2));
+                Assert.AreEqual(2, SumOfASequence.SequenceSum(2, 2, 1));
+                Assert.AreEqual(35, SumOfASequence.SequenceSum(1, 15, 3));
+                Assert.AreEqual(0, SumOfASequence.SequenceSum(15, 1, 3));
+            }
+        }
+    }
+}
